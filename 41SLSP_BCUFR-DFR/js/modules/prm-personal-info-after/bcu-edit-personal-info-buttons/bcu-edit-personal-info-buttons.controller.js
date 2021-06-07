252598ba53d4b985989c5b5abd41a3e93a80694c
Thomas Henkel <thomas.henkel@unifr.ch>
2021-06-07 09:23:20 +0200

    missing 'else' for true > fals case added

--- 41SLSP_BCUFR-DFR/js/modules/prm-personal-info-after/bcu-edit-personal-info-buttons/bcu-edit-personal-info-buttons.controller.js
+++ 41SLSP_BCUFR-DFR/js/modules/prm-personal-info-after/bcu-edit-personal-info-buttons/bcu-edit-personal-info-buttons.controller.js
@@ -1,6 +1,6 @@
 export class bcuEditPersonalInfoButtonsController {
     constructor() {
-        this.detailsBaseEdu = "https:\/\/eduid.ch";
+        this.detailsBaseEdu = "https:\/\/test.eduid.ch";
         this.detailsBaseReg = "https:\/\/registration.slsp.ch\/library-card\/";
         this.exclude = ['STAFF', '11', '12', '13', '14', '15', '16', '91', '92'];
     }
@@ -10,6 +10,9 @@ export class bcuEditPersonalInfoButtonsController {
             let patron = this.afterCtrl.parentCtrl.personalInfoService.personalInfo.patronstatus[0].registration[0].institution[0].patronstatuscode;
             if (!this.exclude.includes(patron)) {
                 return true;
+            }
+            else {
+                return false;
             }
         }
         return false;
