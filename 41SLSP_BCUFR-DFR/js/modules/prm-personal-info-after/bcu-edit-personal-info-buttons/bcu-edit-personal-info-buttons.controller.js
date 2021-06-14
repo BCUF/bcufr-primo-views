3658a365bc32220b0b37490032bc698816e2b387
Thomas Henkel <thomas.henkel@unifr.ch>
2021-06-14 11:21:49 +0200

    update

--- 41SLSP_BCUFR-DFR/js/modules/prm-personal-info-after/bcu-edit-personal-info-buttons/bcu-edit-personal-info-buttons.controller.js
+++ 41SLSP_BCUFR-DFR/js/modules/prm-personal-info-after/bcu-edit-personal-info-buttons/bcu-edit-personal-info-buttons.controller.js
@@ -1,21 +1,25 @@
 export class bcuEditPersonalInfoButtonsController {
     constructor() {
-        this.detailsBaseEdu = "https:\/\/test.eduid.ch";
+        this.detailsBaseEdu = "https:\/\/eduid.ch";
         this.detailsBaseReg = "https:\/\/registration.slsp.ch\/library-card\/";
-        this.exclude = ['STAFF', '11', '12', '13', '14', '15', '16', '91', '92'];
+        this.grpA = ['11', '91', '92'];
+        this.grpB = ['12', '13', '14', '15', '16'];
+        this.showButtons = this.showA = this.showB = false;
     }
 
-    getPatronGrp () {
+    $doCheck () {
         if (this.afterCtrl.parentCtrl.personalInfoService.personalInfo !== undefined) {
             let patron = this.afterCtrl.parentCtrl.personalInfoService.personalInfo.patronstatus[0].registration[0].institution[0].patronstatuscode;
-            if (!this.exclude.includes(patron)) {
-                return true;
+            if (this.grpA.includes(patron)) {
+                this.showA = true;
+            }
+            else if (this.grpB.includes(patron)) {
+                this.showB = true;
             }
             else {
-                return false;
+                this.showButtons = true;
             }
         }
-        return false;
     }
 
 }
